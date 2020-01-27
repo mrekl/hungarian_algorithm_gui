82fd04888add7e0684e730aaffbf9db575022445
mrekl <marcin7m0@gmail.com>
2020-01-28 00:28:27 +0100

    make simple gui

--- main.py
+++ main.py
@@ -1,68 +1,70 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.screenmanager import ScreenManager, Screen
-
+from tkinter import *
 from hungarian_algorithm.main import Munkres
 
-Builder.load_file('view/app.kv')
+tk = Tk()
 
-matrixSizeY = 0
-matrixSizeY = 0
+matrixInputs = []
 
-# temp
-costs = [
-    [30, 25, 10],
-    [15, 10, 20],
-    [25, 20, 15],
-]
+matrixSizeX = None
+matrixSizeY = None
 
-costsValues = None
-costsSum = None
+def updateMatrixInputs():
+    global matrixInputs
+    global matrixSizeX
+    global matrixSizeY
 
-class MenuScreen(Screen):
-    
-    def onCreateMatrixClick(self):
-        global matrixSizeX 
-        matrixSizeX = self.ids.sizeX.text
-        global matrixSizeY
-        matrixSizeY = self.ids.sizeY.text
-        sm.current = 'matrix'
+    resultLbl.grid_remove()
 
-class MatrixScreen(Screen):
-    
-    def onCalculateClick(self):
-        global costsSum
-        costsSum = Munkres(costs)
-        sm.current = 'result'
+    matrixInputs = []
 
-class ResultScreen(Screen):
-    pass
+    sizeX = matrixSizeX = int(sizeXInput.get())
+    sizeY = matrixSizeY = int(sizeYInput.get())
 
-sm = ScreenManager()
-sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(MatrixScreen(name='matrix'))
-sm.add_widget(ResultScreen(name='result'))
+    for i in range(sizeY):
+        matrixInputs.append([])
+        for j in range(sizeX):
+            matrixInputs[i].append(Entry(tk))
+            matrixInputs[i][j].grid(row = i + 2, column = j)
 
-class HungarianAlgorithmApp(App):
+def calculate():
+    costsMatrix = []
 
-    def build(self):
-        return sm
+    for i, row in enumerate(matrixInputs):
+        
+        costsMatrix.append([])
+        for item in row:
+            if(item.get() == ""):
+                costsMatrix[i].append(0)
+            else:
+                costsMatrix[i].append(float(item.get()))
 
-if __name__ == "__main__":
-    HungarianAlgorithmApp().run()
+    costs = Munkres(costsMatrix)
+    costs.calculate()
 
-# def main():
-#     costs = [
-#         [30, 25, 10],
-#         [15, 10, 20],
-#         [25, 20, 15],
-#     ]
+    resultLbl = Label(text = "The optimal value equals: " + str(costs.getSumOfMinCosts()))
+    resultLbl.grid(row = matrixSizeY + 3, column = 0)
 
-#     costs = Munkres(costs)
-#     print(costs.getMinCostsValues())
-    
-# main()
+Label(text = "Matrix size:").grid(row = 0, column = 0)
+sizeXInput = Entry(tk)
+sizeXInput.grid(row = 0, column = 1)
+Label(text = "x").grid(row = 0, column = 2)
+sizeYInput = Entry(tk)
+sizeYInput.grid(row = 0, column = 3)
 
+createMatrixBtn = Button(text = "Create matrix / Clear", command = updateMatrixInputs)
+createMatrixBtn.grid(row = 0, column = 4)
+
+calculateBtn = Button(text = "Calculate", command = calculate)
+calculateBtn.grid(row = 0, column = 5)
+
+Label(text = "").grid(row = 1, column = 0)
+
+resultLbl = Label()
+
+
+
+# btnPrintHello = Button(tk, text = "Print Hello", command = printHello)
+# btnPrintHello.grid(row = 0)
+
+# updateMatrixInputs(3, 4)
+tk.mainloop()
