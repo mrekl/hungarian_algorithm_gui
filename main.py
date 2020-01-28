071081e6a3f04d725a882738fda731847ac95f38
mrekl <marcin7m0@gmail.com>
2020-01-28 09:32:46 +0100

    bugfix

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ tk = Tk()
 
 matrixInputs = []
 
-matrixSizeX = None
-matrixSizeY = None
+matrixSizeX = 5
+matrixSizeY = 5
 
 def updateMatrixInputs():
     global matrixInputs
@@ -26,20 +26,22 @@ def updateMatrixInputs():
             matrixInputs[i].append(Entry(tk))
             matrixInputs[i][j].grid(row = i + 2, column = j)
 
-def calculate():
-    costsMatrix = []
+def calculateOptimalCost():
+    costs = []
 
     for i, row in enumerate(matrixInputs):
         
-        costsMatrix.append([])
+        costs.append([])
         for item in row:
             if(item.get() == ""):
-                costsMatrix[i].append(0)
+                costs[i].append(0)
             else:
-                costsMatrix[i].append(float(item.get()))
+                costs[i].append(int(item.get()))
 
-    costs = Munkres(costsMatrix)
+    costs = Munkres(costs)
     costs.calculate()
+
+    print(costs.getSumOfMinCosts())
 
     resultLbl = Label(text = "The optimal value equals: " + str(costs.getSumOfMinCosts()))
     resultLbl.grid(row = matrixSizeY + 3, column = 0)
@@ -54,17 +56,11 @@ sizeYInput.grid(row = 0, column = 3)
 createMatrixBtn = Button(text = "Create matrix / Clear", command = updateMatrixInputs)
 createMatrixBtn.grid(row = 0, column = 4)
 
-calculateBtn = Button(text = "Calculate", command = calculate)
+calculateBtn = Button(text = "Calculate", command = calculateOptimalCost)
 calculateBtn.grid(row = 0, column = 5)
 
 Label(text = "").grid(row = 1, column = 0)
 
 resultLbl = Label()
 
-
-
-# btnPrintHello = Button(tk, text = "Print Hello", command = printHello)
-# btnPrintHello.grid(row = 0)
-
-# updateMatrixInputs(3, 4)
 tk.mainloop()
